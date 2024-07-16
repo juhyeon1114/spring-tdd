456710c8480f5a04267fc71ae50cb4229ead9e62
juhyeon1114 <juhyeon1114@gmail.com>
2024-07-16 14:37:02 +0900

    init

---
+++ src/test/java/study/spring_tdd/SpringTddApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring_tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
